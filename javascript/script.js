48ec6438050a0124c53000bb1138249fdd507c4f
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-07 20:34:09 +0800

    create a reference of the toast box

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll(".buttons button");
- 
+const toastBox = document.querySelector("#toast-box");
+
+
 buttons.forEach((button) => {
   button.addEventListener("click", showToast);
 })
