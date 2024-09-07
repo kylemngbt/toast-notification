5e247b99995e5c8c98ef6f25db8280b4d57f750b
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-07 20:49:36 +0800

    fix: modify the showToast func

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,25 @@
 const buttons = document.querySelectorAll(".buttons button");
 const toastBox = document.querySelector("#toast-box");
+const successMsg = "Successfully submitted";
+const errorMsg = "Please fix the error!";
+const invalidMsg = "Invalid input, check again"
 
-function showToast() {
+function showToast(e) {
   let toast = document.createElement("div");
   toast.classList.add("toast");
-  toast.innerHTML = "Success";
+  switch(e.target.id) {
+    case "success":
+      toast.innerHTML = successMsg;
+      break;
+    
+    case "error":
+      toast.innerHTML = errorMsg;
+      break;
+
+    case "invalid":
+      toast.innerHTML = invalidMsg;
+      break;
+  }
   toastBox.appendChild(toast);
 }
 
