df4427d7f04c32152ae1705035a94a1563fb4e46
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-07 20:35:17 +0800

    feat: define the initial showToast func

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,12 @@
 const buttons = document.querySelectorAll(".buttons button");
 const toastBox = document.querySelector("#toast-box");
 
+function showToast() {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = "Success";
+  toastBox.appendChild(toast);
+}
 
 buttons.forEach((button) => {
   button.addEventListener("click", showToast);
